cdd9ec2bdcff858115877df583a96f7b730f55aa
Jorge Montellanos Díaz <jorgemontellanos@gmail.com>
2018-06-02 16:05:00 -0400

    escuchador

--- app/src/main/java/com/factorybyte/demo/MainActivity.java
+++ app/src/main/java/com/factorybyte/demo/MainActivity.java
@@ -1,24 +1,76 @@
 package com.factorybyte.demo;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.factorybyte.demo.helpers.BottomNavigationViewHelper;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //definimos varibles
     private BottomNavigationView navigationView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //conectamos con la vista
         navigationView = findViewById(R.id.navigation);
 
+        BottomNavigationViewHelper.disableShiftMode(navigationView);
+
+        textView = findViewById(R.id.titleApp);
+
+
+        initConfig();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.navigation_health:
+                        textView.setText(getResources().getString(R.string.navigation_health));
+                        return true;
+                    case R.id.navigation_sports:
+                        textView.setText(getResources().getString(R.string.navigation_sports));
+                        return true;
+                    case R.id.navigation_education:
+                        textView.setText(getResources().getString(R.string.navigation_education));
+                        return true;
+                    case R.id.navigation_violence:
+                        textView.setText(getResources().getString(R.string.navigation_violence));
+                        return true;
+                    case R.id.navigation_travel:
+                        textView.setText(getResources().getString(R.string.navigation_travel));
+                        return true;
+
+                }
+
+
+                return false;
+            }
+        });
 
 
 
+
+
+
+    }
+
+    private void initConfig() {
+
+        textView.setText(getResources().getString(R.string.navigation_education));
+
+        navigationView.setSelectedItemId(R.id.navigation_education);
     }
 }
